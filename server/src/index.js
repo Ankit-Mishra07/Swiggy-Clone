781a3332b648a9652c79f506a883fa95936d1ad2
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-17 22:14:13 +0530

    register model is created

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,14 @@
 const express = require("express")
-
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
+
+
+
+
 
 
 module.exports = app
---
+++ server/src/models/register.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const registerSchema = new mongoose.Schema({
+    name : {type : String, required: true},
+    mobile : {type : String, required: true, minlength : 10, maxlength : 10},
+    email : {type : String, required : true, unique : true},
+    password : {type : String, required : true},
+    referral : {type : String, required : false}
+})
+
+module.exports = mongoose.model('register', registerSchema)
