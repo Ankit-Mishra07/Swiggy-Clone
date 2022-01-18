bd0fa7ffeb63c29e5ba4e9fab4758c68893aa4dd
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-18 13:32:15 +0530

    working on products

---
+++ client/src/components/Products.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import style from '../Styles/products.module.css'
+import SideNav from './SideNav'
+const Products = () => {
+
+    
+
+    return (
+        <>
+
+        <div className={style.products_container}>
+            <div>
+
+            </div>
+            <div className={style.products_container_child}>
+
+            </div>
+        </div>
+            
+        </>
+    )
+}
+
+export default Products
