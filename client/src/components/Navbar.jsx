985f26e23f1dfbb3ad2897dd45417870583082f5
Vipul chodhari <87421931+Vipchoudhary13@users.noreply.github.com>
2022-01-19 14:32:26 +0530

    navbar jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import style from "../styles/Navbar.module.css";
+
+export const Navbar = () => {
+    return <div className={style.navbar}>
+        <div className={style.left}>
+            <img src="https://logosandtypes.com/wp-content/uploads/2021/01/Swiggy.png"/>
+            <p><u>Work</u></p>
+            <i className="fas fa-angle-down"></i>
+        </div>
+        <div className={style.right}>
+            <i className="fas fa-search"><span>Search</span></i>
+            <i class="fas fa-percentage"><span>Offers</span></i>
+            <i class="far fa-life-ring"><span>Help</span></i>
+            <i class="fas fa-user"><span>Sign In</span></i>
+            <i class="fas fa-shopping-cart"><span>Cart</span></i>
+        </div>
+    </div>
+}
