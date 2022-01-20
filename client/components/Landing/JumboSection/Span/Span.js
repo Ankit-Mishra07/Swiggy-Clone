bacdbbb721973f3f9cb37e9682fc1bd74188acc4
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-20 23:56:39 +0530

    wrong place

--- client/components/Landing/JumboSection/Span/Span.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Span(props) {
-    return (
-        <span>{props.text}</span>
-    )
-}
--- client/components/Landing/Restaurants/Image/Image.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Image.css'
-
-export default function Image(props) {
-    return (
-       <img src={props.src} className={props.imgcls} alt=""/>
-    )
-}
