2d4a68716a76c8f87aa4ecdd0041dadbd525fcb5
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2022-01-23 14:31:10 +0530

    deleted

--- swiggyReact/src/Components/Input/Input.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../Input/Input.css'
-
-export default function Input() {
-    return (
-        <input type="text" placeholder="Enter your delivery location"></input>
-    )
-}
